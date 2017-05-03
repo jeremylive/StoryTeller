767ae0709ade8cdda0b42ef354b58aa02498c49a
Jeremy Live <jeremylive@live.com>
2017-05-03 13:19:42 -0600

    Primer commit, proyecto en eclipse

---
+++ StoryTeller/src/storyteller/interfaz/Interfaz.java
@@ -0,0 +1,22 @@
+package storyteller.interfaz;
+/*Importes de bibliotecas*/
+
+/*
+ * Clase Interfaz
+ * 
+ */
+public class Interfaz {
+	/*Variables Globales*/
+	
+	/*Constructor*/
+	public Interfaz()
+	{
+		
+	}
+	
+	
+	/*Paint*/
+	
+	/*PaintComponent*/
+}
+
